cbdaf91094ac5830bc2a9b097d5649c2aef3f480
Tony Bento <bentotony1010@yahoo.com>
2019-08-08 23:40:01 -0500

    simple test

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 This app will take a serch result and find games with that search result and find cheap prices for that game.
-
-looking into having the back round change???
---
+++ script.js
@@ -0,0 +1,49 @@
+//const url = `http://www.cheapshark.com/api/1.0/deals?storeID=6&desc=2&title=`
+// const api = ``
+
+function generateHtml(obj) {
+    console.log(obj)
+    return `
+    <div class="game">
+        <img src="${obj.thumb}" alt="game img"></img>
+       <h2>${obj.title}</h2>
+       <ul>
+            <li><a href="https://www.metacritic.com${obj.metacriticLink}" target="_blank">Link to Metacritic to learn more about the game.</a></li>
+            <li><p>Is on sale for $${obj.salePrice}</p></li>
+            <li><p>Normal price is $${obj.normalPrice}</p></li>
+            <li><p>You will save ${obj.savings}%</p></li>
+            <li><p>Most of steam rate this game ${obj.steamRatingText}</p></li>
+            <li><p>The steam percetage is ${obj.steamRatingPercent}%</p></li>
+            <li><p>The Metacritic score is ${obj.metacriticScore}%</p></li>
+            <li><p>The deal is rated ${obj.dealRating}/10</p></li>
+            <li><a href="http://www.cheapshark.com/redirect?dealID=${obj.dealID}" target="_blank">Link to deal.</a></li>
+        </ul>
+    </div>   
+       `
+}
+
+function processGameData(responseData) {
+    const gameDataHTML = responseData.map(function (obj) {
+        return generateHtml(obj)
+    }).join('')
+    $('.display').html(gameDataHTML)
+}
+
+function getGame(searchTerm) {
+    const url = `http://www.cheapshark.com/api/1.0/deals?&desc=2&title=${searchTerm}`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(responseJson => processGameData(responseJson));
+}
+
+
+function main() {
+    $('form').on('submit', function (event) {
+        event.preventDefault()
+        const searchTerm = $(event.currentTarget).find('#search').val()
+        getGame(searchTerm)
+    })
+}
+
+$(main)
