aad04b0a909ee2c1a2e588ca016f2a5770c9a2c1
Tony Bento <bentotony1010@yahoo.com>
2019-08-16 18:38:19 -0500

    ready

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const STATE = {
+    storeID: null
+}
+
 function generateHtml(obj) {
     return `
     <div class="game" >
@@ -5,43 +9,110 @@ function generateHtml(obj) {
             
         </div>
        <h2>${obj.title}</h2>
-       <ul>
-            <li><a href="https://www.metacritic.com${obj.metacriticLink}" target="_blank">Link to Metacritic to learn more about the game.</a></li>
-            <li><p>Is on sale for $${obj.salePrice}.</p></li>
-            <li><p>Normal price is $${obj.normalPrice}.</p></li>
-            <li><p>You will save ${obj.savings}%.</p></li>
-            <li><p>Most of steam rate this game ${obj.steamRatingText}.</p></li>
-            <li><p>The steam percetage is ${obj.steamRatingPercent}%.</p></li>
-            <li><p>The Metacritic score is ${obj.metacriticScore}%.</p></li>
-            <li><p>The deal is rated ${obj.dealRating}/10.</p></li>
-            <li><a href="http://www.cheapshark.com/redirect?dealID=${obj.dealID}" target="_blank">Link to deal.</a></li>
+       <ul class="gameObj">
+            <li><a class="big" href="https://www.metacritic.com${obj.metacriticLink}" target="_blank">Link to Metacritic to learn more about the game.</a></li>
+            <li><p class="bigT" >Is on sale for $${obj.salePrice}.</p></li>
+            <li><p class="bigT" >Normal price is $${obj.normalPrice}.</p></li>
+            <li><p class="bigT" >You will save ${obj.savings}%.</p></li>
+            <li><p class="bigT" >Most of steam rate this game ${obj.steamRatingText}.</p></li>
+            <li><p class="bigT" >The steam percetage is ${obj.steamRatingPercent}%.</p></li>
+            <li><p class="bigT" >The Metacritic score is ${obj.metacriticScore}%.</p></li>
+            <li><p class="bigT" >The deal is rated ${obj.dealRating}/10.</p></li>
+            <li><a class="big" href="https://www.cheapshark.com/redirect?dealID=${obj.dealID}" target="_blank">Link to deal.</a></li>
         </ul>
     </div>   
        `
 }
 
+function gameNotFound() {
+    return `
+    <h2 class="detail">Please make sure you spelled the game correctly Also, make sure the game you are looking for is playable
+            on PC. This app will only find games that are on sale for PC(Steam, Uplay, Origin, ETC...).
+            </h2>
+    `
+}
+
 function processGameData(responseData) {
-    const gameDataHTML = responseData.map(function (obj) {
-        return generateHtml(obj)
-    }).join('')
-    $('.display').html(gameDataHTML)
+    if (responseData && responseData.length) {
+        const gameDataHTML = responseData.map(function (obj) {
+            return generateHtml(obj)
+        }).join('')
+        $('.display').html(gameDataHTML)
+    } else {
+        $('.display').html(gameNotFound)
+    }
+
 }
 
 function getGame(searchTerm) {
-    const url = `https://www.cheapshark.com/api/1.0/deals?&desc=2&sortBy=title&title=${searchTerm}`
-
+    const url = `https://www.cheapshark.com/api/1.0/deals?onSale=1&desc=2&sortBy=Title&title=${searchTerm}`
     fetch(url)
         .then(response => response.json())
         .then(responseJson => processGameData(responseJson));
 }
 
+//this is the extra version 2.0 copy when ready
+function extraHtml(obj) {
+    return `
+    <section class="cards">
+        <img class="banner" src="https://www.cheapshark.com${obj.images.banner}" alt="stoe icon">
+        <ul class="deals" id="${obj.storeID}">
+        </ul>
+    </section>
+    `
+}
+
+function dealsHTML(obj) {
+    return `
+    <li><div class="leftSide"><a class="small" href="http://www.cheapshark.com/redirect?dealID=${obj.dealID}" target="_blank"
+    style=":hover {content: url(${obj.thumb});}">${obj.title}</a>
+    </div><div class="rightSide"><del>$${obj.normalPrice}</del><span class="priceBox">$${obj.salePrice}</span></div></li>
+    `
+}
+
+function processExtra(data) {
+    const extaDataHTML = data.map(function (obj) {
+        if (obj.isActive !== 1) {
+            return
+        }
+        return extraHtml(obj)
+    }).join('')
+    $('.info').html(extaDataHTML)
+    STATE.stores = data
+    deals()
+}
+
+function processDeals(dealData, storeID) {
+    const dealsDataHTML = dealData.map(function (obj) {
+        return dealsHTML(obj)
+    }).join('')
+    $('#' + storeID).html(dealsDataHTML)
+}
+
+function extra() {
+    const url2 = `http://www.cheapshark.com/api/1.0/stores`
+    fetch(url2)
+        .then(response => response.json())
+        .then(responseJson => processExtra(responseJson));
+}
+
+function deals() {
+
+    STATE.stores.forEach((element) => {
+        const url3 = `http://www.cheapshark.com/api/1.0/deals?storeID=${element.storeID}&pageSize=7`
+        fetch(url3)
+            .then(response => response.json())
+            .then(responseJson => processDeals(responseJson, element.storeID));
+    });
+}
+
 
 function main() {
+    extra()
     $('form').on('submit', function (event) {
         event.preventDefault()
         const searchTerm = $(event.currentTarget).find('#search').val()
         getGame(searchTerm)
     })
 }
-
 $(main)
